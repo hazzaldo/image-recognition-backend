727981f6156afedf5073c9a0b78916156d0a1850
hazzaldo <hazzaldo@hotmail.com>
2019-01-08 19:42:16 +0000

    create the comments the outline the first set of end-points to be created

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail //post because we're positing some data (e.g. username and passowrd). And we don't want to send password through the request query. So we send it in a request body over HTTPS, so that it's hidden from man-in-the-middle attacks and it's secure. 
+
+/register --> POST = user //post becasue we're adding the data to a DB. Returns new user object.
+
+/profile/:userId --> GET = user //access user profile with optional param of accessing userID, so that each user has their own home screen.
+
+/image --> PUT = user(specifically rank of images uploaded/  /PUT because we're simply updating how many images have been uploaded. Because we want to work with the ranking - i.e. anytime a user posts a new photo, we want to make sure that their count of how many photos they've submitted goes up - so we have a variable that keeps track of score, and then check against other users to see who has submitted the most photos and give them a rank.
+*/
